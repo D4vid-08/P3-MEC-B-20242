36c5ca34c9ab1c2da59078f1ca40c50701bc30be
D4vid-08 <juandavidmello08@gmail.com>
2024-08-27 15:23:39 -0500

    Update pg1.java

--- P1/pg1.java
+++ P1/pg1.java
@@ -1,1 +1,16 @@
+/*Crear una clase llamada fracion con atributos correspondientes a numerador y denominador, aplicar encapsula, y los metodos set y get necesario*/
+public class Triangulo {
+    // Atributos privados
+    private int base ;
+    private int altura;
 
+    
+    public Triangulo(double base , double  altura ) {
+        this.base = base ;
+        this.altura = altura;
+    }
+
+    // Métodos get
+    public int altura() {
+        return altura;
+    }
