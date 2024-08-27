bd73bf3e5aa427e6e7d63706546c7d97e0081117
D4vid-08 <juandavidmello08@gmail.com>
2024-08-27 15:33:16 -0500

    Update pg2.java

--- P1/pg2.java
+++ P1/pg2.java
@@ -1,1 +1,31 @@
+//a Partir de la misma clase crear sobrecarga al contructor y crear dos objetos para cada una de las formas de constructor 
+public class Triangulo {
+    // Atributos privados
+    private int base ;
+    private int altura;
 
+    
+    public Triangulo(int base , int  altura ) {
+        this.base = base ;
+        this.altura = altura;
+    
+    }
+
+    // Métodos get
+    public int getaltura() {
+        return altura;
+    }
+
+    public int getbase() {
+        return base;
+    }
+
+    // Métodos set
+    public void setbase(int base) {
+        this.base = base;
+    }
+
+    public void setaltura(int altura) {
+        this.altura = altura;
+    }
+}
